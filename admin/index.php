a1cab6f92175e40ac7aecd66a1da8507d893cd22
karima-hussein <karima.hussein21@gmail.com>
2020-07-14 21:17:29 +0200

    m

--- admin/index.php
+++ admin/index.php
@@ -1,8 +1,24 @@
 <?php 
     $title='Dashboard';
-    include_once "../includes/admin_header.php"; ?>
-    <!-- Navigation -->
-    <?php include_once "../includes/admin_nav.php"; ?>
+    $path="../";
+    include_once "../includes/admin_header.php"; 
+    include_once "../includes/admin_nav.php"; 
+    include_once "../includes/categories.php"; 
+    include_once "../includes/posts.php"; 
+    include_once "../includes/users.php"; 
+    include_once "../includes/comments.php"; 
+
+    $userObj= new user();
+    $commentObj = new comment();
+    $postObj = new post();
+    $catObj = new category();
+    
+    $catCount = mysqli_fetch_row($catObj->count())[0];
+    $commentCount = mysqli_fetch_row($commentObj->count())[0];
+    $postCount = mysqli_fetch_row($postObj->count())[0];
+    $userCount = mysqli_fetch_row($userObj->count())[0];
+    
+?>
     <div id="page-wrapper">
         <div class="container-fluid">
             <!-- Page Heading -->
@@ -17,6 +33,7 @@
             </div>
             <!-- /.row -->
         </div>
+        
         <!-- /.container-fluid -->
         <div class="row">
             <div class="col-lg-3 col-md-6">
@@ -27,7 +44,7 @@
                                 <i class="fa fa-file-text fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                                <div class='huge'>12</div>
+                                <div class='huge'><?php echo $postCount;?></div>
                                 <div>Posts</div>
                             </div>
                         </div>
@@ -49,7 +66,7 @@
                                 <i class="fa fa-comments fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                                <div class='huge'>23</div>
+                                <div class='huge'><?php echo $commentCount;?></div>
                                 <div>Comments</div>
                             </div>
                         </div>
@@ -71,7 +88,7 @@
                                 <i class="fa fa-user fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                                <div class='huge'>23</div>
+                                <div class='huge'><?php echo $userCount;?></div>
                                 <div> Users</div>
                             </div>
                         </div>
@@ -93,7 +110,7 @@
                                 <i class="fa fa-list fa-5x"></i>
                             </div>
                             <div class="col-xs-9 text-right">
-                                <div class='huge'>13</div>
+                                <div class='huge'><?php echo $catCount;?></div>
                                 <div>Categories</div>
                             </div>
                         </div>
@@ -108,8 +125,39 @@
                 </div>
             </div>
         </div>
-                <!-- /.row -->
+        <!-- /.row -->
+        <!-- chart -->
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+        <script type="text/javascript">
+            google.charts.load('current', {'packages':['bar']});
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var data = google.visualization.arrayToDataTable([
+                ['components', 'counts'],
+                //getting value with php
+                <?php
+                    $component=['posts','comments','users','categories'];
+                    $counts=[$postCount,$commentCount,$userCount,$catCount];
+                    for($i=0;$i < sizeof($component);$i++){
+                        echo "['$component[$i]', $counts[$i]],";
+                    }
+                 ?>
+                ]);
+
+                var options = {
+                chart: {
+                    title: 'Components stats',
+                    subtitle: '',
+                }
+                };
+
+                var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                chart.draw(data, google.charts.Bar.convertOptions(options));
+            }
+        </script>
+        <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
     </div>
     <!-- /#page-wrapper -->
 <?php include_once "../includes/admin_footer.php"; ?>
-
